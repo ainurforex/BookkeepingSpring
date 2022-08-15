2bbd347033c116a730e8b390b727598eefd2eed8
Ainur <ainurforex@mail.ru>
2022-08-15 13:03:32 +0500

    Initial commit

---
+++ src/main/java/ru/ainurforex/BookkeepingSpring/BookkeepingSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.ainurforex.BookkeepingSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookkeepingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookkeepingSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ainurforex/BookkeepingSpring/BookkeepingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ainurforex.BookkeepingSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookkeepingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
