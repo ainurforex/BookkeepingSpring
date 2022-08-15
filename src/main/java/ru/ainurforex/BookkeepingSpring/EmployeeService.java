658365c46e9b2ca69677a3e36b8a36d73506a95c
Ainur <ainurforex@mail.ru>
2022-08-16 02:46:12 +0500

    Курс 2 ДЗ 6 переделал после уточнения Но метод contains не работет

--- src/main/java/ru/ainurforex/BookkeepingSpring/EmployeeService.java
+++ src/main/java/ru/ainurforex/BookkeepingSpring/EmployeeService.java
@@ -14,67 +14,48 @@ public class EmployeeService {
         this.employeesBook = new ArrayList<>();
     }
 
-    @Override
-    public String toString() {
-        String stringEmployeeBook = "";
-        for (int i = 0; i < employeesBook.size(); i++) {
-            if (employeesBook.get(i) != null) {
-                stringEmployeeBook = stringEmployeeBook + "N:" + i +
-                        " First name:" + employeesBook.get(i).getFirstName() +
-                        " Last name:" + employeesBook.get(i).getLastName() + "\n";
-            }
-
-
+    public Employee addEmployee(String firstname, String lastName) {
+        Employee employee = new Employee(firstname, lastName);
+        if (findEmployeeInList(employee) >= 0) {
+            throw new EmployeeAlreadyAddedException("Работник с данными параметрами уже существует");
         }
-        return stringEmployeeBook;
+
+        employeesBook.add(employee);
+        return employee;
     }
 
-    public void addEmployee(Employee employee) {
-
-        try {
-            if (findEmployee(employee) == true) {
-                throw new EmployeeAlreadyAddedException("Работник с данными параметрами уже существует");
-            }
-        } catch (EmployeeNotFoundException e) {
-            employeesBook.add(employee);
-        }
-    }
-
-    public void removeEmployee(Employee employee) {
-        for (int i = 0; i < employeesBook.size(); i++) {
-            if (employeesBook.get(i) == null) {
-                continue;
-            }
-            if (employeesBook.get(i).equals(employee)) {
-                employeesBook.set(i, null);
-                break;
-            }
-            if (i == employeesBook.size() - 1) {
-                throw new EmployeeNotFoundException("Работник с данными параметрами не найден.");
-            }
+    public Employee removeEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        int index = findEmployeeInList(employee);
+        if (index >= 0) {
+            employeesBook.set(index, null);
+            return employee;
         }
 
-    }
-
-    public boolean findEmployee(Employee employee) {
-        for (int i = 0; i < employeesBook.size(); i++) {
-            if (employeesBook.get(i) == null) {
-                continue;
-            }
-            if (employeesBook.get(i).equals(employee)) {
-                return true;
-            }
-
-        }
         throw new EmployeeNotFoundException("Работник с данными параметрами не найден.");
     }
 
-    public Employee getEmployeeForIndex(int employeeIndex) {
-        try {
-            return employeesBook.get(employeeIndex);
-        } catch (NullPointerException e) {
-            throw new IndexNotFound();
+
+    public Employee findEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        int index = findEmployeeInList(employee);
+        if (index >= 0) {
+            return employee;
         }
+
+        throw new EmployeeNotFoundException("Работник с данными параметрами не найден.");
+    }
+
+    private int findEmployeeInList(Employee employee) {
+        for (int i = 0; i < employeesBook.size(); i++) {
+            if (employeesBook.get(i) == null) {
+                continue;
+            }
+            if (employeesBook.get(i).equals(employee)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public List printAllEmployee() {
